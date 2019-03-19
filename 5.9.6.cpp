ccd83f423167cb9dcfbe851906d43833ced3e241
berieo <ycs19990204@outlook.com>
2019-03-19 09:30:02 +0800

    03/19

---
+++ 5.9.6.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+
+int main() {
+	char ch[10][10];
+	char c[5];
+	int i = 0;
+	/*
+	while (1) {
+		scanf_s("%s", ch[i]);
+		if (ch[i] == "Quit") break;
+		else printf("%s", *ch[i++]);
+	}
+	*/
+	scanf_s("%s", c);
+	return 0;
+}
